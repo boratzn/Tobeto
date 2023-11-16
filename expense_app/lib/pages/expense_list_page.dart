d7ecc7e88906b27da276aee16a6260381f21ae84
boratzn <boratzn94@gmail.com>
2023-11-16 13:24:50 +0300

    validation eklendi

--- expense_app/lib/pages/expense_list_page.dart
+++ expense_app/lib/pages/expense_list_page.dart
@@ -22,6 +22,7 @@ class _ExpenseListPageState extends State<ExpenseListPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     final expModel = context.read<ExpenseModel>();
     DateTime? selectedDate;
 
@@ -69,133 +70,156 @@ class _ExpenseListPageState extends State<ExpenseListPage> {
                     left: 16,
                     right: 16,
                     bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: SizedBox(
-                  height: 410,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      const Text(
-                        "Hareket Ekle",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      //*********************AÇIKLAMA****************** */
-                      TextField(
-                        autofocus: true,
-                        controller: _controllerTitle,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.purple)),
-                          labelText: "Açıklama",
+                child: Form(
+                  key: _formKey,
+                  child: SizedBox(
+                    height: 410,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const Text(
+                          "Hareket Ekle",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      //*********************MİKTAR********************** */
-                      TextField(
-                        keyboardType: TextInputType.number,
-                        autofocus: true,
-                        controller: _controllerPrice,
-                        decoration: const InputDecoration(
-                          suffix: Text("₺"),
-                          hintText: "Miktar",
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.purple)),
-                          labelText: "Miktar",
-                        ),
-                      ),
-                      //*********************TARİH SEÇİMİ****************************
-                      Row(
-                        children: [
-                          const Text(
-                            "Tarih seçiniz: ",
-                            style: TextStyle(fontSize: 16),
+                        //*********************AÇIKLAMA****************** */
+                        TextFormField(
+                          autofocus: true,
+                          controller: _controllerTitle,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.purple)),
+                            labelText: "Açıklama",
                           ),
-                          Expanded(
-                            child: TextField(
-                              controller: _controllerDate,
-                              decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(25),
-                                    borderSide: const BorderSide(
-                                      color: Colors.purple,
-                                    ),
-                                  ),
-                                  suffixIcon: IconButton(
-                                      onPressed: () async {
-                                        final DateTime? dt =
-                                            await showDatePicker(
-                                                context: context,
-                                                initialDate: DateTime.now(),
-                                                firstDate: DateTime(1900),
-                                                lastDate: DateTime(2100));
-
-                                        if (dt != null) {
-                                          expModel.selectedDate = dt;
-                                          _controllerDate.text = DateFormat(
-                                                  'd/M/y')
-                                              .format(expModel.selectedDate!)
-                                              .toString();
-                                        }
-                                      },
-                                      icon: const Icon(Icons.date_range)),
-                                  //enabled: false,
-                                  labelText: "Seçilen Tarih",
-                                  hintText: selectedDate.toString()),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Açıklama Kısmı Boş Bırakılamaz!!";
+                            }
+                            return null;
+                          },
+                        ),
+                        //*********************MİKTAR********************** */
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          autofocus: true,
+                          controller: _controllerPrice,
+                          decoration: const InputDecoration(
+                            suffix: Text("₺"),
+                            hintText: "Miktar",
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.purple)),
+                            labelText: "Miktar",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Miktar Kısmı Boş Bırakılamaz!!";
+                            }
+                            return null;
+                          },
+                        ),
+                        //*********************TARİH SEÇİMİ****************************
+                        Row(
+                          children: [
+                            const Text(
+                              "Tarih seçiniz: ",
+                              style: TextStyle(fontSize: 16),
                             ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          //*********************KATEGORİ SEÇİMİ****************************
-                          const Text("Kategori: ",
-                              style: TextStyle(fontSize: 16)),
-                          Expanded(
-                            child: DropdownButtonHideUnderline(
-                                child: DropdownButtonFormField(
-                              decoration: InputDecoration(
-                                  border: OutlineInputBorder(
+                            Expanded(
+                              child: TextFormField(
+                                controller: _controllerDate,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
                                       borderRadius: BorderRadius.circular(25),
                                       borderSide: const BorderSide(
                                         color: Colors.purple,
-                                      ))),
-                              isExpanded: true,
-                              items: Category.values.map((e) {
-                                return DropdownMenuItem(
-                                  value: e,
-                                  child: Text(e.name.toString()),
-                                );
-                              }).toList(),
-                              value: expModel.category,
-                              onChanged: (value) {
-                                expModel.category = value!;
-                              },
-                            )),
-                          ),
-                        ],
-                      ),
-                      //*******************KAYDET BUTONU********************** */
-                      ElevatedButton(
-                          style: ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all(Colors.white),
-                              backgroundColor: const MaterialStatePropertyAll(
-                                  Colors.purple)),
-                          onPressed: () {
-                            expModel.addExpense(Expense(
-                              name: _controllerTitle.text,
-                              price: double.parse(_controllerPrice.text),
-                              date: selectedDate ?? DateTime.now(),
-                              category: expModel.category,
-                            ));
-                            Navigator.pop(context);
-                            _controllerTitle.clear();
-                            _controllerPrice.clear();
-                            _controllerDate.clear();
-                          },
-                          child: const Text(
-                            "Kaydet",
-                          ))
-                    ],
+                                      ),
+                                    ),
+                                    suffixIcon: IconButton(
+                                        onPressed: () async {
+                                          final DateTime? dt =
+                                              await showDatePicker(
+                                                  context: context,
+                                                  initialDate: DateTime.now(),
+                                                  firstDate: DateTime(1900),
+                                                  lastDate: DateTime(2100));
+
+                                          if (dt != null) {
+                                            expModel.selectedDate = dt;
+                                            _controllerDate.text = DateFormat(
+                                                    'd/M/y')
+                                                .format(expModel.selectedDate!)
+                                                .toString();
+                                          }
+                                        },
+                                        icon: const Icon(Icons.date_range)),
+                                    //enabled: false,
+                                    labelText: "Seçilen Tarih",
+                                    hintText: selectedDate.toString()),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return "Lütfen Tarih Seçiniz!!";
+                                  }
+                                  return null;
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            //*********************KATEGORİ SEÇİMİ****************************
+                            const Text("Kategori: ",
+                                style: TextStyle(fontSize: 16)),
+                            Expanded(
+                              child: DropdownButtonHideUnderline(
+                                  child: DropdownButtonFormField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                        borderSide: const BorderSide(
+                                          color: Colors.purple,
+                                        ))),
+                                isExpanded: true,
+                                items: Category.values.map((e) {
+                                  return DropdownMenuItem(
+                                    value: e,
+                                    child: Text(e.name.toString()),
+                                  );
+                                }).toList(),
+                                value: expModel.category,
+                                onChanged: (value) {
+                                  expModel.category = value!;
+                                },
+                              )),
+                            ),
+                          ],
+                        ),
+                        //*******************KAYDET BUTONU********************** */
+                        ElevatedButton(
+                            style: ButtonStyle(
+                                foregroundColor:
+                                    MaterialStateProperty.all(Colors.white),
+                                backgroundColor: const MaterialStatePropertyAll(
+                                    Colors.purple)),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                expModel.addExpense(Expense(
+                                  name: _controllerTitle.text,
+                                  price: double.parse(_controllerPrice.text),
+                                  date: selectedDate ?? DateTime.now(),
+                                  category: expModel.category,
+                                ));
+                                Navigator.pop(context);
+                                _controllerTitle.clear();
+                                _controllerPrice.clear();
+                                _controllerDate.clear();
+                              }
+                            },
+                            child: const Text(
+                              "Kaydet",
+                            ))
+                      ],
+                    ),
                   ),
                 ),
               );
